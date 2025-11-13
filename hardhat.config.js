18638a854b544725ae5bf29ba339110e64f8a1b9
Coruja-Dev <guilhermepdl97@protonmail.com>
2025-11-13 20:35:53 -0300

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
+  paths: {
+    sources: "./challenges",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
